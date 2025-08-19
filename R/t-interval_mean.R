aba6da83677d7b73ca4db2aa9a67f32aaac7f29c
Michal <michal.stadnik-12@wp.pl>
2025-08-19 08:56:59 +0200

    feat: add welchs_t_interval function

--- R/t-interval_mean.R
+++ R/t-interval_mean.R
@@ -29,3 +29,24 @@ two_sample_pooled_t_interval <- function(sample_1, sample_2, conf_level=0.95){
   mean_diff <- sample_1_mean - sample_2_mean
   interval <- c(lower=mean_diff-margin_error, upper=mean_diff+margin_error, margin_error=margin_error)
 }
+
+welchs_t_interval <- function(sample_1, sample_2, conf_interval=0.95){
+  sample_1_mean <- mean(sample_1)
+  sample_2_mean <- mean(sample_2)
+  s_1 <- sd(sample_1)
+  s_2 <- sd(sample_2)
+  n <- length(sample_1)
+  m <- length(sample_2)
+  alpha <- 1 - conf_level
+  
+  waged_sum_of_variances <- s_1^2/n + s_2^2/m
+  r_component_1 <- (s_1^2/n)^2/(n-1)
+  r_component_2 <- (s_2^2/m)^2/(m-1)
+  
+  r <- floor(waged_sum_of_variances^2/(r_component_1+r_component_2))
+  
+  t_quantile <- qt(alpha/2, r, lower.tail=FALSE)
+  margin_error <- t_quantile*sqrt(waged_sum_of_variances)
+  mean_diff <- sample_1_mean - sample_2_mean
+  interval <- c(lower=mean_diff-margin_error, upper=mean_diff+margin_error, margin_error=margin_error)
+}
